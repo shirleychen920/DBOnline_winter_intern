37ff01d57c5b241d8a4758c24c5b2f5c38a5bc27
dbyen <dbyen@media.ucla.edu>
2016-01-30 22:24:21 -0800

    Fix how the menu opens

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.menu-icon').click(function() {
 	$('.menu').animate({left: '0px'});
-	$('.bars').animate({left: '0px'});
+	$('.menu-icon.bars').animate({left: '0px'});
 	});
 	
 });
